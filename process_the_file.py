2284f135d7a4c633c0325e8b696f6d5a97579b79
10125852 <10125852@mydbs.ie>
2017-05-02 22:28:28 +0100

    Add files via upload

---
+++ process_the_file.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+date: 02/05/2017
+author: 10125852 InSun Ahn
+
+"""
+
+# Open the file.
+myfile = "test_short.txt"
+
+# Read the file by lines and strip the lines.
+data = [line.strip() for line in open(myfile, 'r')]
+
+# Print the number of lines.
+print(len(data))
+
+# Define class for commit
+

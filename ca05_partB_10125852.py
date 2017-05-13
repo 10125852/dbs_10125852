69d3490851a849528a6ae210d7203b266364d476
10125852 <10125852@mydbs.ie>
2017-05-13 18:33:31 +0100

    Added above_mean and below_mean functions.

--- ca05_partB_10125852.py
+++ ca05_partB_10125852.py
@@ -9,6 +9,8 @@ author: 10125852 InSun Ahn
 CA05 Part B: Refactoring calculator using lambda, map, reduce, filter functions
 '''
 
+import math
+
 class calculator(object):
     ''' class for 10 calculator functions to handle listed values.'''
   
@@ -32,6 +34,17 @@ class calculator(object):
     def is_even(self, values):
         return filter(lambda x: x % 2 == 0, self.values)
     
-        # Return only odd numbers among listed values.
+    # Return only odd numbers among listed values.
     def is_odd(self, values):
+        return filter(lambda x: x % 2 == 1, self.values)
+    
+    # Return the values that are above mean in a list.
+    def above_mean(self, values, mean):
+        return filter(lambda x: x > mean, values)
+    
+    # Return the values that are below mean in a list.
+    def below_mean(self, values, mean):
+        return filter(lambda x: x < mean, values)    
+    
+
         

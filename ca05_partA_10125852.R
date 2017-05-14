5e7c98d208eaadf855660a79143a769f9bbbf833
10125852 <10125852@mydbs.ie>
2017-05-14 10:16:03 +0100

    Fixed syntax errors with last 4 functions.

--- ca05_partA_10125852.R
+++ ca05_partA_10125852.R
@@ -37,20 +37,20 @@ my_sqrt <- function(x){
 
 # Trigonometric Functions 7-9
 ## Function 7 - Sine
-my_sin(x) <- function(x){
+my_sin <- function(x){
   sin(x)
 }
 ## Function 8 - Cosine
-my_cos(x) <- function(x){
+my_cos <- function(x){
   cos(x)
 }
 ## Function 9 - Tangent
-my_tan(x) <- function(x){
+my_tan <- function(x){
   tan(x)
 }
 
 # Function 10 - Compute logarithms of a value/vector x and base = y. 
-my_log(x) <- function(x){
+my_log <- function(x, y){
   logb(x, base = y)
 }
 
@@ -62,13 +62,31 @@ d <- c(5, 6, 7, 8)
 
 my_addition(a, b)
 my_addition(c, d)
+
 my_subtraction(a, b)
 my_subtraction(c, d)
+
 my_multiplication(a, b)
 my_multiplication(c, d)
+
 my_division(a, b)
 my_division(c, d)
+
 my_power(a, b)
 my_power(c, d)
+
 my_sqrt(25)
 my_sqrt(c)
+
+my_sin(b)
+my_sin(d)
+
+my_cos(a)
+my_cos(c)
+
+my_tan(b)
+my_tan(d)
+
+my_log(100, base = 2)
+my_log(a, base = b)
+my_log(d, base = c)

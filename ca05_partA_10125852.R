a9c821a5a72aa5260616fa1955f8ca7047635205
10125852 <10125852@mydbs.ie>
2017-05-14 09:56:22 +0100

    Created an R file for CA02-Part A.

---
+++ ca05_partA_10125852.R
@@ -0,0 +1,41 @@
+# Data Analytics Group 1
+# 10125852 InSun Ahn
+
+# CA05 - PART A 
+# Calculator Program
+
+
+# Function 1 - compute summation of two values/vectors.
+my_addition <- function(x,y) {
+  x + y
+}
+
+# Function 2 - compute subtraction of two values/vectors.
+my_subtraction <- function(x,y){
+  x - y
+}
+
+# Function 3 - compute multiplication of two values/vectors.
+my_multiplication <- function(x,y){
+  x * y
+}
+
+# Function 4 - compute division of two values/vectors.
+my_division <- function(x,y){
+  x / y
+}
+
+
+
+
+
+
+a = 7
+b = 5
+c <- c(1, 2, 3, 4)
+d <- c(5, 6, 7, 8)
+
+my_addition(a, b)
+my_addition(c, d)
+my_subtraction(a, b)
+my_subtraction(c, d)
